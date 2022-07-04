48007ddb0d0ae341f4ca5a9b0e551f2d69ee0125
ZHAO Xudong <zxdong@gmail.com>
2022-07-05 07:52:58 +0800

    Support cors (#26)

--- src/app.js
+++ src/app.js
@@ -1,32 +1,41 @@
-
 import express from 'express'
-import bodyParser from 'body-parser'
 import morgan from 'morgan'
-import { Service } from './models/Service'
+import {
+  Service
+} from './models/Service'
 import verify from './utils/verify'
+import { nanoid } from 'nanoid'
+import createApi from './utils/create-api'
 
-const app = express()
 const {
   UPGRADE_SERVER_API_PREFIX
 } = process.env
 
+const app = express()
 app.use(morgan('tiny'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(express.json({
+  limit: '50mb'
+}))
 
 app.get(['/test', '/'], (req, res) => res.send('server running'))
+
 app.post('/update-upgrade-info-' + UPGRADE_SERVER_API_PREFIX, async (req, res) => {
   let sig = req.get('X-Hub-Signature')
   if (!sig) {
     return res.status(401).send('require signature header')
   }
-  let { body } = req
+  const {
+    body
+  } = req
   sig = sig.replace(/^sha1=/, '')
-  let result = verify(body, sig)
+  const result = verify(body, sig)
   if (!result) {
     return res.status(400).send('signature not match')
   }
-  let {
+  const {
     action,
     release: {
       tag_name: tagName,
@@ -36,13 +45,13 @@ app.post('/update-upgrade-info-' + UPGRADE_SERVER_API_PREFIX, async (req, res) =
       name
     }
   } = body
-  let indb = await Service.findOne({
+  const indb = await Service.findOne({
     where: {
       id: name
     }
   })
   if (action !== 'published') {
-    return res.status(400).send('not a published event')
+    return res.status(200).send('not a published event')
   }
   if (!indb) {
     await Service.create({
@@ -65,12 +74,14 @@ app.post('/update-upgrade-info-' + UPGRADE_SERVER_API_PREFIX, async (req, res) =
   res.send('ok')
 })
 
-app.get('/upgrade-info', async (req, res) => {
-  let { name } = req.query
+async function upgradeInfo (req, res) {
+  const {
+    name
+  } = req.query
   if (!name) {
     return res.status(400).send('app name required')
   }
-  let inst = await Service.findOne({
+  const inst = await Service.findOne({
     where: {
       id: name
     }
@@ -79,6 +90,69 @@ app.get('/upgrade-info', async (req, res) => {
     return res.status(404).send('app not find')
   }
   res.send(inst)
+}
+
+createApi(app, '/upgrade-info', upgradeInfo)
+
+app.get('/upgrade-info', async (req, res) => {
+  const {
+    name
+  } = req.query
+  if (!name) {
+    return res.status(400).send('app name required')
+  }
+  const inst = await Service.findOne({
+    where: {
+      id: name
+    }
+  })
+  if (!inst) {
+    return res.status(404).send('app not find')
+  }
+  res.send(inst)
+})
+
+app.post('/download', async (req, res) => {
+  const {
+    image,
+    filename = 'rc.png'
+  } = req.body
+  if (!image) {
+    return res.status(400).send('bad request')
+  }
+  const id = nanoid()
+  await Service.sync()
+  await Service.create({
+    id,
+    log: image,
+    version: filename
+  })
+  res.send({
+    id
+  })
+})
+
+app.get('/download', async (req, res) => {
+  const {
+    id
+  } = req.query
+  if (!id) {
+    return res.status(400).send('bad request')
+  }
+  const inst = await Service.findOne({
+    where: {
+      id
+    }
+  })
+  if (!inst) {
+    return res.status(400).send('bad request')
+  }
+  const base64Data = inst.log.replace(/^data:image\/png;base64,/, '')
+  const img = Buffer.from(base64Data, 'base64')
+  res.type('png')
+  res.set('Content-Disposition', 'attachment; filename="rc.png"')
+  res.set('Content-Length', img.length)
+  res.end(img)
 })
 
 export default app
